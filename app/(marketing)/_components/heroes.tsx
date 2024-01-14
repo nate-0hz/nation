41d0e89fdd0a1dade2e76dfa6f79f867bcbc5d97
Nate P <33781520+nate-0hz@users.noreply.github.com>
2024-01-14 15:17:11 +1100

    complete landing page
    - change colour scheme (incomplete)
    - change colour of images

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -2,22 +2,34 @@ import Image from 'next/image';
 
 export const Heroes = () => {
   return (
-    <div className="flex flex-col items-centre justify-centre max-w-5xl">
+    <div className="flex flex-col items-centre justify-centre max-w-5xl pt-10">
       <div className="flex items-centre">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
           <Image
-            src="/documents-xprnt.png"
+            src="/documents-sky-950.png"
             alt="Man struggling with a lot of documents"
             layout="fill"
-            className="object-contain"
+            className="object-contain dark:hidden"
+          />
+          <Image
+            src="/documents-slate-300.png"
+            alt="Man struggling with a lot of documents"
+            layout="fill"
+            className="object-contain hidden dark:block"
           />
         </div>
         <div className="relative w-[400px] h-[400px] hidden md:block">
           <Image 
-            src="/reading-xprnt.png"
+            src="/reading-sky-950.png"
             alt="Unhappy, young woman reading a book, with two stacks of books beside her"
             layout="fill"
-            className="object-contain"
+            className="object-contain dark:hidden"
+          />
+          <Image 
+            src="/reading-slate-300.png"
+            alt="Unhappy, young woman reading a book, with two stacks of books beside her"
+            layout="fill"
+            className="object-contain hidden dark:block"
           />
         </div>
       </div>
--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
   return (
     <div className={cn(
       "z-50 bg-background fixed top-0 flex items-center w-full p-6",
-      scrolled && "border-b shadow-sm"
+      scrolled && "border-b border-slate-400 shadow-sm"
     )}>
       <Logo />
       <div className=" md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
