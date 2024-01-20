1464bdf1bef3a12a4783a50ac3eea3ddbf1007fc
Nate P <33781520+nate-0hz@users.noreply.github.com>
2024-01-21 08:50:01 +1100

    Sidebar Animations
    - Complete sidebar animations
    - Add useEffect Hooks for determining sidebar collapse on mobile v desktop
    - Add documents page
    - Add navigation page
    - Add main layout
    - Add loading spinner on landing page
    - Add login buttons on landing page

---
+++ app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,10 @@
+const DocumentsPage = () => {
+  return (
+    <div>
+      <h1>Documents</h1>
+      This is a protected page
+    </div>
+  );
+}
+
+export default DocumentsPage
--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   return (
     <div className="max-w-3xl space-y-4 font-sans">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold leading-relaxed">
@@ -12,10 +17,26 @@ export const Heading = () => {
       <h3 className="text-base sm:text-xl md:text-2xl font-medium">
         nation brings all your thoughts together, without a monthly fee.
       </h3>
-      <Button>
-        Enter Nation 
-        <ArrowRight  className="h-4 w-4 ml-2"/>
-      </Button>
+      {isLoading && (
+        <div className="w-full flex items-center justify-center">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Get into Nation 
+            <ArrowRight  className="h-4 w-4 ml-2"/>
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Get Nation for free
+          </Button>
+        </SignInButton>
+      )}
     </div>
   )
 }
