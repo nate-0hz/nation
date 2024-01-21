c0f7dd1083b3b3c0e970f7c77a65b202f46428a3
Nate P <33781520+nate-0hz@users.noreply.github.com>
2024-01-21 14:05:08 +1100

    Sidebar item
    - Add create page functionality in sidebar
    - Add user-item.tsx
    - Add instances of items on navigation within the sidebar - Search and Settings

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,8 +1,21 @@
 "use client"
 
-import { ChevronsLeft, MenuIcon } from "lucide-react"
+import { 
+  ChevronsLeft, 
+  MenuIcon, 
+  PlusCircle, 
+  Search, 
+  Settings 
+} from "lucide-react"
+
+import { 
+  ElementRef, 
+  useEffect, 
+  useRef, 
+  useState 
+} from "react";
+
 import { usePathname } from "next/navigation";
-import { ElementRef, useEffect, useRef, useState } from "react";
 
 /* because sidebar is manually draggable, not east to cater for with Tailwind
 * use of useMediaQuery hook from usehooks-ts to detect screen size */
@@ -10,8 +23,10 @@ import { useMediaQuery } from "usehooks-ts";
 
 import { cn } from "@/lib/utils";
 import { UserItem } from "./user-item";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
+import { Item } from "./item";
+import { toast } from "sonner";
 
 
 export const Navigation = () => {
@@ -19,6 +34,7 @@ export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery("(max-width: 768px)");
   const documents = useQuery(api.documents.get);
+  const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -104,6 +120,16 @@ export const Navigation = () => {
     }
   }
 
+  const handleCreate = () => {
+    const promise = create({ title: "Untitled" });
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created.",
+      error: "Failed to create a new note."
+    })
+  }
+
   return (
     <>
       <aside 
@@ -126,7 +152,26 @@ export const Navigation = () => {
         >
           <ChevronsLeft className="h-6 w-6" />
         </div>
-        <UserItem />
+        <div>
+          <UserItem />
+          <Item
+            label="Search" 
+            icon={Search}
+            isSearch
+            onClick={() => {}}
+          />
+          <Item
+            label="Settings" 
+            icon={Settings}
+            onClick={() => {}}
+          />
+          <Item
+            onClick={handleCreate} 
+            label="New page" 
+            icon={PlusCircle} 
+          />
+
+        </div>
         <div className="mt-4">
           {documents?.map((document) => (
             <p key={document._id}>
