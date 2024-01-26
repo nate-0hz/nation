ec16246f17eb23423c8b1c77489cb2ad25d6e8df
Nate P <33781520+nate-0hz@users.noreply.github.com>
2024-01-27 10:28:44 +1100

    Main View - add icon
    - Add icon-picker
    - Add toolbar
    - Add to db
    - Allow select and remove icon
    - Add cover image button

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,9 +1,49 @@
-const DocumentIdPage = () => {
-  return (
-    <div>
-      Document Id
-    </div>
-  );
+"use client"
+
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdPageProps {
+  // equiv of using useParams hook. takes documentId from the [documentId] dynamic route foldername, also in the path
+  params: {
+    documentId: Id<"documents">;
+  }
 }
 
-export default DocumentIdPage
+const DocumentIdPage = ({
+  params
+}: DocumentIdPageProps) => {
+  const document =  useQuery(api.documents.getById, {
+    documentId: params.documentId
+  });
+
+  if (document === undefined) {
+    return (
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
+  if (document === null) {
+    return (
+      <div>
+        Not found
+      </div>
+    )
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
+};
+
+export default DocumentIdPage;
