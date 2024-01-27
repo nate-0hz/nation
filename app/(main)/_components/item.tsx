b57b59e303231db18c6adadfbd282203d20bc4a4
Nate P <33781520+nate-0hz@users.noreply.github.com>
2024-01-27 21:21:30 +1100

    Publish
    - Add 404 handling
    - Add use-origin hook
    - Add public layout
    - Add publish and unpublish functionality

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -6,7 +6,7 @@ import {
   LucideIcon,
   MoreHorizontal,
   Plus,
-  Trash
+  Trash,
 } from "lucide-react";
 
 import {
@@ -14,7 +14,7 @@ import {
   DropdownMenuTrigger,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuSeparator
+  DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 
 import { Skeleton } from "@/components/ui/skeleton";
@@ -24,11 +24,9 @@ import { cn } from "@/lib/utils";
 
 import { useMutation } from "convex/react";
 
-
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 import { useUser } from "@clerk/clerk-react";
-
 
 interface ItemProps {
   id?: Id<"documents">;
@@ -41,7 +39,7 @@ interface ItemProps {
   onClick?: () => void;
   icon: LucideIcon;
   expanded?: boolean; // Add the expanded property
-};
+}
 
 export const Item = ({
   id,
@@ -60,61 +58,56 @@ export const Item = ({
   const create = useMutation(api.documents.create);
   const archive = useMutation(api.documents.archive);
 
-  const onArchive =  (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-    ) => {
-      event.stopPropagation();
-      if (!id) return;;
-      const promise = archive({ id })
-        .then(() => router.push("/documents"));
+  const onArchive = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    event.stopPropagation();
+    if (!id) return;
+    const promise = archive({ id }).then(() => router.push("/documents"));
 
-      toast.promise(promise, {
-        loading: "Moving to trash...",
-        success: "Note moved to trash.",
-        error: "Failed to move note to trash."
-      });
-    }
+    toast.promise(promise, {
+      loading: "Moving to trash...",
+      success: "Note moved to trash.",
+      error: "Failed to move note to trash.",
+    });
+  };
 
   const handleExpand = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
   ) => {
     event.stopPropagation();
     onExpand?.();
-  }
+  };
 
-  const onCreate = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>
-  ) => {
+  const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     event.stopPropagation();
-    if (!id)  return;
-    const promise = create({title: "Untitled", parentDocument: id})
-    .then((documentId) => {
-      if (!expanded) {
-        onExpand?.();
-      }
-      router.push(`/documents/${documentId}`)
-    });
+    if (!id) return;
+    const promise = create({ title: "Untitled", parentDocument: id }).then(
+      (documentId) => {
+        if (!expanded) {
+          onExpand?.();
+        }
+        router.push(`/documents/${documentId}`);
+      },
+    );
 
     toast.promise(promise, {
       loading: "Creating a new note...",
       success: "New note created.",
-      error: "Failed to create a new note."
-    })
-  }
+      error: "Failed to create a new note.",
+    });
+  };
 
   const ChevronIcon = expanded ? ChevronDown : ChevronRight;
-
-
 
   return (
     <div
       onClick={onClick}
       role="button"
-      style={{ 
-        paddingLeft: level? `${(level * 12) + 12}px` : "12px" }}
+      style={{
+        paddingLeft: level ? `${level * 12 + 12}px` : "12px",
+      }}
       className={cn(
         "group min-h-[27px] text-sm py-1 pr-3 w-full hover:bg-primary/5 flex items-center text-muted-foreground font-medium",
-        active && "bg-primary/5 text-primary"
+        active && "bg-primary/5 text-primary",
       )}
     >
       {!!id && (
@@ -123,23 +116,15 @@ export const Item = ({
           className="h-full rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 mr-1"
           onClick={handleExpand}
         >
-          <ChevronIcon 
-            className="h-4 w-4 shrink-0 text-muted-foreground/50" 
-          />
+          <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
       )}
       {documentIcon ? (
-        <div className="shrink-0 mr-2 text-[18px]">
-          {documentIcon}
-        </div>  
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon 
-          className="shrink-0 h-[18px] mr-2 text-muted-foreground" 
-        />
+        <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
       )}
-      <span className="trucate">
-        {label}
-      </span>
+      <span className="trucate">{label}</span>
       {isSearch && (
         <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
           <span className="text-xs">⌘</span>K
@@ -148,10 +133,7 @@ export const Item = ({
       {!!id && (
         <div className="ml-auto flex items-center gap-x-2">
           <DropdownMenu>
-            <DropdownMenuTrigger
-              onClick={(e) => e.stopPropagation()}
-              asChild
-            >
+            <DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
               <div
                 role="button"
                 className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
@@ -166,7 +148,7 @@ export const Item = ({
               forceMount
             >
               <DropdownMenuItem onClick={onArchive}>
-                <Trash className="h-4 w-4 mr-2"/>
+                <Trash className="h-4 w-4 mr-2" />
                 Delete
               </DropdownMenuItem>
               <DropdownMenuSeparator />
@@ -185,19 +167,19 @@ export const Item = ({
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-Item.Skeleton = function ItemSkeleton({ level }: {level?: number }) {
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
   return (
     <div
       style={{
-        paddingLeft: level ? `${(level * 12) + 25}px` : "12px"
+        paddingLeft: level ? `${level * 12 + 25}px` : "12px",
       }}
       className="flex gap-x-2 py-[3px]"
     >
-      <Skeleton className="h-4 w-4"/>
-      <Skeleton className="h-4 w-[30%]"/>
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
     </div>
-  )
-}
+  );
+};
--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -29,7 +29,7 @@ const MainLayout = ({
   }
 
   return (
-    <div className="h-full flex dark:bg-[#1F1F1F]">
+    <div className="h-full flex dark:bg-[#1F1F1F] dark:text-[#FFFFFF]">
       <Navigation />
       <main className="flex-1 h-full overflow-y-auto">
         <SearchCommand />
