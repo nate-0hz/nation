1c19c63e03319d54dbc0dabee79768d20c440c3f
Nate P <33781520+nate-0hz@users.noreply.github.com>
2024-01-27 19:52:44 +1100

    Editor component
    - install blocknote
    - add editor
    - add redirects to documents on create/delete

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
@@ -16,9 +18,23 @@ interface DocumentIdPageProps {
 }
 
 const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  // dynamic import of editor component, setting server side rendering to false
+  const Editor = useMemo(
+    () => dynamic(() => import("@/components/editor"), { ssr: false }),
+    [],
+  );
   const document = useQuery(api.documents.getById, {
     documentId: params.documentId,
   });
+
+  const update = useMutation(api.documents.update);
+
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    });
+  };
 
   if (document === undefined) {
     return (
@@ -26,10 +42,10 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
         <Cover.Skeleton />
         <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
           <div className="space-y-4 pl-8 pt-4">
-            <Skeleton className="h-14 w-[50%]"/>
-            <Skeleton className="h-4 w-[80%]"/>
-            <Skeleton className="h-4 w-[40%]"/>
-            <Skeleton className="h-4 w-[60%]"/>
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
           </div>
         </div>
       </div>
@@ -45,6 +61,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
       <Cover url={document.coverImage} />
       <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
       </div>
     </div>
   );
--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -61,12 +61,12 @@ export const Item = ({
   const archive = useMutation(api.documents.archive);
 
   const onArchive =  (
-
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
     ) => {
       event.stopPropagation();
       if (!id) return;;
-      const promise = archive({ id });
+      const promise = archive({ id })
+        .then(() => router.push("/documents"));
 
       toast.promise(promise, {
         loading: "Moving to trash...",
@@ -92,7 +92,7 @@ export const Item = ({
       if (!expanded) {
         onExpand?.();
       }
-      // router.push(`/documents/${documentId}`)
+      router.push(`/documents/${documentId}`)
     });
 
     toast.promise(promise, {
