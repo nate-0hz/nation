ed3488793c27cd516c22dc9483551b28c549f29c
Nate P <33781520+nate-0hz@users.noreply.github.com>
2024-02-12 05:47:43 +1100

    Update auth.config.js
    - Reflect PROD URL

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -2,8 +2,13 @@
 
 export default {
   providers: [
+    // DEV:
+    // {
+    //   domain: "https://modern-terrier-9.clerk.accounts.dev",
+    //   applicationID: "convex",
+    // }
     {
-      domain: "https://modern-terrier-9.clerk.accounts.dev",
+      domain: "https://useful-warbler-621.convex.cloud",
       applicationID: "convex",
     }
   ]
